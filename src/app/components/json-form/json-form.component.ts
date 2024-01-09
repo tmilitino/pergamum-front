11cf9e35a201bea82f605d7bf94b97515d382ed4
thiago militino <tmilitino@arsenal>
2024-01-09 02:20:13 -0300

    add json form componet

---
+++ src/app/components/json-form/json-form.component.ts
@@ -0,0 +1,145 @@
+import { Component, ViewChild, ElementRef, AfterViewInit, Renderer2, Input, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Output, EventEmitter } from '@angular/core';
+import { JsonFormUtils } from '../json-form-utils';
+
+
+declare global {
+  interface Window {
+    JSONEditor: any;
+  }
+}
+
+
+@Component({
+  selector: 'app-json-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './json-form.component.html',
+  styleUrl: './json-form.component.scss',
+})
+
+export class JsonFormComponent implements AfterViewInit, OnInit {
+
+  @ViewChild('jsonform') jsonform!: ElementRef<HTMLInputElement>;
+  @Output()
+  change = new EventEmitter<Event>()
+  jsonEditor: any = undefined;
+
+  @Input() schema: any = {}
+
+  @Input() data: any = {}
+
+
+  @Input() enabled: boolean = true
+
+  @Input() title: string = 'JSON Form Component From Darshan'
+
+  private editorOptions = {
+    iconlib: "fontawesome5",
+    object_layout: "normal",
+    schema: this.schema || {},
+    show_errors: "interaction",
+    theme: "bootstrap4",
+
+    startval: this.data || {},
+  };
+
+  inter!: NodeJS.Timer;;
+
+
+  constructor(private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document) {
+    this.setUpEditor.bind(this);
+    this.initJsoneditor.bind(this);
+    this.validate.bind(this)
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+
+  setUpEditor() {
+    if (this.jsonEditor) {
+      this.jsonEditor.destroy();
+    }
+
+    this.editorOptions.schema = this.schema;
+
+    this.editorOptions.startval = this.data;
+
+    this.jsonEditor = new window.JSONEditor(
+      this.jsonform.nativeElement,
+      this.editorOptions
+    );
+    console.log("Inside SetupEditor", this.jsonEditor)
+    this.jsonEditor.on("change", () => {
+      if (this.validate()) {
+        const value = this.jsonEditor.getValue();
+        console.warn(value, "value")
+
+        this.change.emit(value);
+      }
+
+    });
+    this.jsonEditor.on("ready", () => {
+      // Now the api methods will be available
+
+      if (this.enabled !== true) {
+        this.jsonEditor.disable();
+      }
+    });
+    console.log(this.jsonEditor)
+  }
+
+  validate() {
+    const errors = this.jsonEditor.validate();
+
+    return errors.length === 0;
+  }
+
+  initJsoneditor() {
+    // destroy old JSONEditor instance if exists
+
+
+    if (window.JSONEditor) {
+      this.setUpEditor();
+    } else {
+
+      this.inter = setInterval(() => {
+        console.warn(window.JSONEditor)
+
+        if (window.JSONEditor && typeof this.editorOptions !== 'string') {
+          clearInterval(this.inter as unknown as number);
+          this.setUpEditor();
+        }
+      }, 3000);
+    }
+    // new instance of JSONEditor
+
+    // listen for changes
+  };
+
+
+
+  LoadExternerDeps() {
+    const doc = new JsonFormUtils(this.document);
+    doc.loadJsScript(this.renderer, "https://cdn.jsdelivr.net/npm/@json-editor/json-editor@latest/dist/jsoneditor.min.js");
+    doc.loadCss(this.renderer, "https://use.fontawesome.com/releases/v5.6.1/css/all.css");
+    doc.loadCss(this.renderer, "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css")
+  }
+
+  ngAfterViewInit() {
+
+
+
+    if (this.jsonform !== undefined) {
+
+      this.LoadExternerDeps()
+      this.initJsoneditor();
+
+    }
+  }
+}
