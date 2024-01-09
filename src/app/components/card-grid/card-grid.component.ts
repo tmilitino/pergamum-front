8fb54611fdb57adc562edfafad383e955e881d70
thiago militino <tmilitino@arsenal>
2024-01-09 02:42:23 -0300

    add component card grid

---
+++ src/app/components/card-grid/card-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-grid.component.html',
+  styleUrl: './card-grid.component.scss'
+})
+export class CardGridComponent {
+
+}
