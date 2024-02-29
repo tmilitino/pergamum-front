02f4cabcbe61174801f2e63876775dd6e7261c20
tmilitino <tmilitino.tm@gmail.com>
2024-02-29 01:18:08 -0300

    add json editor in app

--- src/app/components/contract-schema-edit/contract-schema-edit.component.ts
+++ src/app/components/contract-schema-edit/contract-schema-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { JsonFormComponent } from '../json-form/json-form.component';
 
 @Component({
   selector: 'app-contract-schema-edit',
   standalone: true,
-  imports: [],
+  imports: [JsonFormComponent],
   templateUrl: './contract-schema-edit.component.html',
   styleUrl: './contract-schema-edit.component.scss'
 })
